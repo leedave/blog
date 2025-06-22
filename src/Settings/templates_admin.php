413301313619d6d8adeba5067f065ed0fd2cb388
David Lee <david.lee@arcmedia.ch>
2025-06-22 12:43:04 +0200

    Fix template dir

--- src/Settings/templates_admin.php
+++ src/Settings/templates_admin.php
@@ -14,7 +14,7 @@ if (!defined('templates_blog_admin')) {
     define('template_blog_admin_entry_row_image', $templateDir . "/blog/entry/row_image.html");
 
     define('jsAdminTemplates', [
-        "blog_tag_editor" => leedch_admin_templateFolder . "js/blog/tagEditor.html",
-        "blog_tag_item" => leedch_admin_templateFolder . "js/blog/tagItem.html",
+        "blog_tag_editor" => $templateDir . "js/blog/tagEditor.html",
+        "blog_tag_item" => $templateDir . "js/blog/tagItem.html",
     ]);
 }
